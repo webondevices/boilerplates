1109ccf7216011b3953feab4257dcc73b5dd8579
Mate Marschalko <webondevices@gmail.com>
2017-03-12 12:59:03 +0000

    Added react-es2015-scss boilerplate

---
+++ react-es2015-scss/src/jsx/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return <div>
+					<div>Hello world, happy Hannukah!</div>
+					<img src="images/han.jpg" alt=""/>
+
+				</div>;
+	}
+}
+
+export default App;
