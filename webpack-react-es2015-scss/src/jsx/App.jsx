cb1c2ea6a146bb79f26dd5816a45e6ae46759bc8
Mate Marschalko <mate.marschalko@miceanddice.com>
2017-05-05 10:48:33 +0100

    Renamed and duplicated

---
+++ webpack-react-es2015-scss/src/jsx/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hi!</h1>
+				<img src="images/html5.png" alt=""/>
+			</div>
+		);
+	}
+}
+
+export default App;
