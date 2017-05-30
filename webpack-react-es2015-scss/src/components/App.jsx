f9bbb1163d45b60f36efba3ce40bd81106d8640b
Mate Marschalko <webondevices@gmail.com>
2017-05-30 09:45:04 +0100

    New webpack react boilerplate

--- webpack-react-es2015-scss/src/components/App.jsx
+++ webpack-react-es2015-scss/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import style from './App.scss';
+import html5image from './../images/html5.png';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Hi!</h1>
-				<img src="images/html5.png" alt=""/>
+				<h1>Hello!</h1>
+				<img src={html5image} alt=""/>
 			</div>
 		);
 	}
